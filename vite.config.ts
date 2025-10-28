5c77fce425445e3c5653dc20c362fea1367be420
Muhammed Rishan k <rishanmpdy@gmail.com>
2025-10-28 19:29:44 +0530

    Update vite.config.ts with new configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // IMPORTANT: set base to your repo name for GitHub Pages
+      base: '/myPortfolio/',
       server: {
         port: 3000,
         host: '0.0.0.0',
